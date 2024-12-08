9ca71e813dae7f7104ce612ac94af88ffd766218
James Drew <james.mcw.drew@gmail.com>
2024-12-08 18:13:07 +0000

    Add Handicap scopes
    See #3

--- app/Models/Handicap.php
+++ app/Models/Handicap.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Handicap extends Model
@@ -15,4 +16,22 @@ class Handicap extends Model
         'set_score'       => 'integer',
         'set_allowance'   => 'integer',
     ];
+
+    // Scopes ----
+
+    /**
+     * Find appropriate handicaps for the given match score
+     */
+    public function scopeForMatchScore(Builder $query, int $score): Builder
+    {
+        return $query->where('match_score', '<=', $score);
+    }
+
+    /**
+     * Find appropriate handicaps for the given set score
+     */
+    public function scopeForSetScore(Builder $query, int $score): Builder
+    {
+        return $query->where('set_score', '<=', $score);
+    }
 }
